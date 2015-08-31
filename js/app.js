49f26ff5ed06bcba3d2d7535f2deacfb55aa182d
phpdevdn <khacduy0103@gmail.com>
2015-08-31 16:50:48 +0700

    31-8

--- js/app.js
+++ js/app.js
@@ -1,6 +1,24 @@
 var mainApp = angular.module('mainApp',[]);
-mainApp.controller('product',['$scope','$http',function($scope,$http){
-	$http.get('data/product.json').success(function(data){
+mainApp.filter('my_date',function() {
+	function convert_date(data){
+		 var $date = new Date(data);
+		return $date.getTime();
+	}
+	return convert_date;
+});
+mainApp.controller('product',['$scope','$http','$filter',function($scope,$http,$filter){
+	$http.get('data/product.json').success(function(data){		
+		angular.forEach(data,function( value, key ){
+			data[key].time=$filter('my_date')(data[key].time);
+		});
+		$scope.order_prod="time";
+		$scope.asc= true; 
 		$scope.prods = data ;
+		$scope.s_name = 4 ;
+		$scope.s_type = 'title';
+		$scope.s_search = { 'id' : $scope.s_name };
+		$scope.order_sort=function(data){
+			$scope.asc = (data==1)? false : true ;
+		}
 	});
  }]);
